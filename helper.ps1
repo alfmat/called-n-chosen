18e7fff6d29c0a7cf605d860cd475f94e86b1597
Alfred Mathew <alfredmathew718@gmail.com>
2021-07-31 12:35:24 -0400

    added another option to ps script

--- helper.ps1
+++ helper.ps1
@@ -1,12 +1,15 @@
+$term = "d"
+
 Write-Output "Welcome Matt!"
 
+# options
 Write-Output "1 to view changed files"
 Write-Output "2 to commit changes"
 Write-Output "3 to reset changes"
-
+Write-Output "4 to get latest code"
 
 do {
-    $decision = Read-Host "What would you like to do? (enter done to terminate)"
+    $decision = Read-Host "What would you like to do? (enter $term to terminate)"
     if($decision -eq 1) {
         git status
     } elseif ($decision -eq 2) {
@@ -19,11 +22,14 @@ do {
         if(Read-Host "Are you sure, you want to reset your current work (y/n): " -eq "y") {
             git reset --hard
         }        
-    } else {
-        if ($decision -ne "done") {
+    } elseif ($decision -eq 4) {
+        git pull --rebase origin main
+    } 
+    else {
+        if ($decision -ne $term) {
             Write-Output "invalid input, try again Matt!"
         }
     }
-} while ($decision -ne "done")
+} while ($decision -ne $term)
 
 Write-Output "All done, thanks Matt!"
