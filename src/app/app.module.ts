0815a8ef84323d72b8a3271735739556961b07c1
Alfred Mathew <alfredmathew718@gmail.com>
2021-09-03 00:42:06 -0400

    added components for nav, footer, contacts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { FooterComponent } from './generic/footer/footer.component';
+import { ContactComponent } from './generic/contact/contact.component';
+import { NavbarComponent } from './generic/navbar/navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FooterComponent,
+    ContactComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule
