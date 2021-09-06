6de2bc33d91653ba7b7fa668d797ff9f9871a06f
Alfred Mathew <alfredmathew718@gmail.com>
2021-09-06 12:52:03 -0400

    angular routing added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,29 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { Routes, RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { FooterComponent } from './generic/footer/footer.component';
 import { ContactComponent } from './generic/contact/contact.component';
 import { NavbarComponent } from './generic/navbar/navbar.component';
+import { HomeComponent } from './pages/home/home.component';
+
+const routes: Routes = [
+  {path: '', pathMatch: 'full', redirectTo: '/home'},
+  {path: 'home', component: HomeComponent }
+]
 
 @NgModule({
   declarations: [
     AppComponent,
     FooterComponent,
     ContactComponent,
-    NavbarComponent
+    NavbarComponent,
+    HomeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
